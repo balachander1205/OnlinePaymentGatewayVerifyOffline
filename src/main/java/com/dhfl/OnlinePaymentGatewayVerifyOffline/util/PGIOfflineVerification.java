dc8267472ffdd67d3de953c7808f0f22dd261722
balachander1205 <digumarthi.bala@gmail.com>
2020-07-25 22:27:10 +0530

    Initial Commit1

--- src/main/java/com/dhfl/OnlinePaymentGatewayVerifyOffline/util/PGIOfflineVerification.java
+++ src/main/java/com/dhfl/OnlinePaymentGatewayVerifyOffline/util/PGIOfflineVerification.java
@@ -3,6 +3,9 @@ package com.dhfl.OnlinePaymentGatewayVerifyOffline.util;
 import com.tp.pg.util.TransactionRequestBean;
 
 public class PGIOfflineVerification {
+	public static void main(String[] args) {
+		getTxNdetails();
+	}
 	public static String getTxNdetails() {
 		String key = "3330290559QVHYCU";
 		String iv = "8168854229WUSGEL";
@@ -10,23 +13,18 @@ public class PGIOfflineVerification {
 		objTransactionRequestBean.setStrRequestType("O");
 		objTransactionRequestBean.setStrMerchantCode("T519384");
 		objTransactionRequestBean.setMerchantTxnRefNumber("TXN112000009781594541927978");
-		objTransactionRequestBean.setStrAmount("1.00");
+		objTransactionRequestBean.setStrAmount("");
 		objTransactionRequestBean.setStrCurrencyCode("INR");
-		// objTransactionRequestBean.setStrITC("");
-		// objTransactionRequestBean.setStrReturnURL("https://www.tekprocess.co.in/MerchantIntegrationClient/Responsepayload.jsp");
-		objTransactionRequestBean.setStrReturnURL("http://localhost:1665/callback");
-		objTransactionRequestBean.setStrS2SReturnURL("http://localhost:1665/callback");
 		objTransactionRequestBean.setStrShoppingCartDetails("FIRST_1.0_0.0");
-		objTransactionRequestBean.setTxnDate("20-01-2020");
-		// objTransactionRequestBean.setStrBankCode("");
+		objTransactionRequestBean.setTxnDate("12-07-2020");
 		objTransactionRequestBean
 				.setWebServiceLocator("https://www.tpsl-india.in/PaymentGateway/services/TransactionDetailsNew");
 		objTransactionRequestBean.setCustID("");
 		objTransactionRequestBean.setStrTPSLTxnID("1165219539");
-		objTransactionRequestBean.setStrMobileNumber("9999999999");
+		objTransactionRequestBean.setStrMobileNumber("");
 		objTransactionRequestBean.setKey(key.getBytes());
 		objTransactionRequestBean.setIv(iv.getBytes());
-		objTransactionRequestBean.setStrCustomerName("xyz");
+		objTransactionRequestBean.setStrCustomerName("");
 		objTransactionRequestBean.setStrEmail("myname@domain.com");
 		String token = objTransactionRequestBean.getTransactionToken();
 		System.out.println("Token :--->> " + token.length());
