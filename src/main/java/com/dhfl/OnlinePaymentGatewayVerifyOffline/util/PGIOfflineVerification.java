b5ac708d0c29f5bb382024de4c1b3d74777ab34c
balachander1205 <digumarthi.bala@gmail.com>
2020-07-25 20:24:10 +0530

    Initial Commit

---
+++ src/main/java/com/dhfl/OnlinePaymentGatewayVerifyOffline/util/PGIOfflineVerification.java
@@ -0,0 +1,35 @@
+package com.dhfl.OnlinePaymentGatewayVerifyOffline.util;
+
+import com.tp.pg.util.TransactionRequestBean;
+
+public class PGIOfflineVerification {
+	public static String getTxNdetails() {
+		String key = "3330290559QVHYCU";
+		String iv = "8168854229WUSGEL";
+		TransactionRequestBean objTransactionRequestBean = new TransactionRequestBean();
+		objTransactionRequestBean.setStrRequestType("O");
+		objTransactionRequestBean.setStrMerchantCode("T519384");
+		objTransactionRequestBean.setMerchantTxnRefNumber("TXN112000009781594541927978");
+		objTransactionRequestBean.setStrAmount("1.00");
+		objTransactionRequestBean.setStrCurrencyCode("INR");
+		// objTransactionRequestBean.setStrITC("");
+		// objTransactionRequestBean.setStrReturnURL("https://www.tekprocess.co.in/MerchantIntegrationClient/Responsepayload.jsp");
+		objTransactionRequestBean.setStrReturnURL("http://localhost:1665/callback");
+		objTransactionRequestBean.setStrS2SReturnURL("http://localhost:1665/callback");
+		objTransactionRequestBean.setStrShoppingCartDetails("FIRST_1.0_0.0");
+		objTransactionRequestBean.setTxnDate("20-01-2020");
+		// objTransactionRequestBean.setStrBankCode("");
+		objTransactionRequestBean
+				.setWebServiceLocator("https://www.tpsl-india.in/PaymentGateway/services/TransactionDetailsNew");
+		objTransactionRequestBean.setCustID("");
+		objTransactionRequestBean.setStrTPSLTxnID("1165219539");
+		objTransactionRequestBean.setStrMobileNumber("9999999999");
+		objTransactionRequestBean.setKey(key.getBytes());
+		objTransactionRequestBean.setIv(iv.getBytes());
+		objTransactionRequestBean.setStrCustomerName("xyz");
+		objTransactionRequestBean.setStrEmail("myname@domain.com");
+		String token = objTransactionRequestBean.getTransactionToken();
+		System.out.println("Token :--->> " + token.length());
+		return token;
+	}
+}
